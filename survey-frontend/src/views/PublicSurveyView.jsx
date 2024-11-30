dab0b642cb23da2e8f295d8791d903562085c8ba
pablocybernetic <gitaup08@gmail.com>
2024-11-30 22:38:23 +0300

    footer

--- survey-frontend/src/views/PublicSurveyView.jsx
+++ survey-frontend/src/views/PublicSurveyView.jsx
@@ -1,95 +1,108 @@
-import {useNavigate, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Axios from "../services/axios.js";
-import {BeatLoader} from "react-spinners";
-import {PublicQuestionComponent} from "../components/surveys/PublicQuestionComponent.jsx";
+import { BeatLoader } from "react-spinners";
+import { PublicQuestionComponent } from "../components/surveys/PublicQuestionComponent.jsx";
 import CustomButton from "../components/core/CustomButton.jsx";
+import Footer from "../Layouts/Footer.jsx";
+import { FiSend } from 'react-icons/fi';  // Fi stands for Feather Icons
 
 const PublicSurveyView = () => {
-  const {slug} = useParams();
-  const [survey, setSurvey] = useState({questions: []})
+  const { slug } = useParams();
+  const [survey, setSurvey] = useState({ questions: [] });
   const [loading, setLoading] = useState(false);
-  const answers = {}
-  const navigate = useNavigate()
+  const answers = {};
+  const navigate = useNavigate();
 
   // fetch survey on component mount
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     Axios.get(`/view/survey/${slug}`)
-      .then(({data}) => {
-        setSurvey(data.data)
-        setLoading(false)
-      })
-  }, [])
+      .then(({ data }) => {
+        setSurvey(data.data);
+        setLoading(false);
+      });
+  }, []);
 
   // handle answer update
   const handleAnswerChange = (value, question) => {
-    answers[question.uuid] = value
-  }
+    answers[question.uuid] = value;
+  };
 
   // handle answers submit
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    // send survey answers
-    Axios.post(`/survey/answer/${survey.answer_id}`, {answers})
-      .then((response) => {
-        navigate('/thanks', {replace: true})
-      }).catch((errors) => {
-      console.log(errors)
-    })
+    Axios.post(`/survey/answer/${survey.answer_id}`, { answers })
+      .then(() => {
+        navigate("/thanks", { replace: true });
+      })
+      .catch((errors) => {
+        console.log(errors);
+      });
   };
 
   return (
-    <div className='min-h-screen bg-gray-100'>
+    <>
+    <div className="min-h-screen bg-white-100">
       {loading && (
-        <div className='m-8 text-center'>
-          <BeatLoader color="#3b82f6"/>
+        <div className="m-8 text-center">
+          <BeatLoader color="#3b82f6" />
         </div>
       )}
 
       {!loading && (
         <div className="w-full p-4 mx-auto sm:p-6 md:w-4/5 lg:w-2/3 xl:w-1/2">
-  {/* Survey Card */}
-  <div className="flex flex-col gap-4 p-6 my-4 text-center rounded-lg shadow-lg bg-gradient-to-r from-blue-50 to-purple-50 md:p-8 md:gap-6 md:my-8">
-    <img 
-      src={survey.image_url} 
-      alt={survey.title} 
-      className="w-full h-auto max-w-xs mx-auto border border-gray-200 rounded-lg shadow-md sm:max-w-sm md:max-w-md lg:max-w-lg"
-    />
-    <h1 className="text-lg font-semibold text-gray-800 sm:text-xl md:text-2xl lg:text-3xl">{survey.title}</h1>
-    <span className="text-xs text-gray-600 sm:text-sm md:text-base">
-      Expire after: <b className="text-red-500">{survey.expire_date}</b>
-    </span>
-    <p className="text-sm leading-relaxed text-gray-700 sm:text-base md:text-lg">{survey.description}</p>
-  </div>
-  
-  {/* Survey Form */}
-  <div className="mt-4 md:mt-6">
-    <form 
-      action="#" 
-      onSubmit={handleSubmit} 
-      className="p-4 space-y-4 bg-white rounded-lg shadow-md sm:p-6 md:p-8 md:space-y-6"
-    >
-      {survey.questions.map((question, index) => (
-        <PublicQuestionComponent 
-          key={question.uuid} 
-          question={question}
-          onAnswerChange={(value) => handleAnswerChange(value, question)} 
-        />
-      ))}
-      <CustomButton 
-        type="submit" 
-        className="w-full px-4 py-2 mt-4 text-white transition duration-200 bg-blue-600 rounded-md hover:bg-blue-700"
-      >
-        Submit
-      </CustomButton>
-    </form>
-  </div>
-</div>
+          {/* Survey Card */}
+          <div className="flex flex-col gap-4 p-6 my-4 text-center rounded-lg shadow-lg bg-gradient-to-r from-blue-50 to-purple-50 md:p-8 md:gap-6 md:my-8">
+            {survey.image_url && (
+              <img
+                src={survey.image_url}
+                alt={survey.title}
+                className="w-full h-auto max-w-full mx-auto border border-gray-200 rounded-lg shadow-md md:max-w-2xl lg:max-w-3xl"
+              />
+            )}
+            <h1 className="text-lg font-semibold text-gray-800 sm:text-xl md:text-2xl lg:text-3xl">
+              {survey.title}
+            </h1>
+            <span className="text-xs text-gray-600 sm:text-sm md:text-base">
+              Expires after: <b className="text-red-500">{survey.expire_date}</b>
+            </span>
+            <p className="text-sm leading-relaxed text-gray-700 sm:text-base md:text-lg">
+              {survey.description}
+            </p>
+          </div>
 
+          {/* Survey Form */}
+          <div className="mt-4 md:mt-6">
+            <form
+              action="#"
+              onSubmit={handleSubmit}
+              className="p-4 space-y-4 bg-white rounded-lg shadow-md sm:p-6 md:p-8 md:space-y-6"
+            >
+              {survey.questions.map((question, index) => (
+                <PublicQuestionComponent
+                  key={question.uuid}
+                  question={question}
+                  onAnswerChange={(value) => handleAnswerChange(value, question)}
+                />
+              ))}
+              <CustomButton
+      type="submit"
+      className="flex items-center justify-center w-full px-4 py-2 mt-4 space-x-2 text-white transition duration-200 bg-blue-600 rounded-md hover:bg-blue-700"
+    >
+      <FiSend className="w-5 h-5" /> {/* The icon */}
+      <span>Submit</span>
+    </CustomButton>
+            </form>
+          </div>
+        </div>
       )}
     </div>
+    <br /><br /><br />
+
+    <Footer/>
+    </>
   );
 };
 
--- survey-frontend/src/views/ThanksView.jsx
+++ survey-frontend/src/views/ThanksView.jsx
@@ -1,11 +1,22 @@
 export const ThanksView = () => {
   return (
-    <div className='p-4 mx-auto mt-24 md:w-1/2'>
+    <div className="p-6 mx-auto mt-24 md:w-1/2">
       <div className="border mockup-window bg-success">
-        <div className="flex justify-center px-4 py-16 bg-green-100">
-          <span className='text-xl font-medium text-green-900'>
-            🎉 Thank you for your time, we appreciate your honest answers.
+        <div className="flex flex-col items-center justify-center px-6 py-16 bg-green-50">
+          <span className="mb-6 text-xl font-medium text-center text-green-900">
+            🎉 Thank you for sharing your valuable time and insights with us. Your feedback is greatly appreciated.
           </span>
+          <a
+            href="https://artcaffemarket.co.ke/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block px-8 py-3 text-sm font-semibold text-green-900 transition-all duration-300 ease-in-out bg-green-200 rounded-full shadow-md hover:bg-green-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700"
+          >
+            Shop Groceries, Bakery, & Wines
+          </a>
+          <p className="mt-4 text-sm text-center text-gray-600">
+            Enjoy a seamless shopping experience with top-quality products, delivered to your doorstep.
+          </p>
         </div>
       </div>
     </div>
